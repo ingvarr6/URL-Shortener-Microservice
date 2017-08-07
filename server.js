58e1a31762e8b0c15796683b87c6fb43ce2ce473
System <none>
2017-08-07 12:37:55 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -35,11 +35,27 @@ app.get('/new/:url*', function(req, res){
 })
 
 app.get('/:id', function(req, res){
-  res.send(req.params.id)
+  var id = req.params.id;
+  // if (!isNaN(Number(id)) && id.length === 4){
+  //   res.send(req.params.id)
+  // }else{
+  //   res.json({"error":"This url is not on the database."});
+  // }
+  
 })
 
 function validateUrl(value) {
   return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
+
+function getUrlFromDb(id){
+  mongo.connect(url, function(err, db) {
+    if (err) throw err;
+    var collection = db.collection('urls');
+    collection.find({
+      short_id: id
+    })
+  })
 }
 
 function saveToDb(short_id, validURL){
