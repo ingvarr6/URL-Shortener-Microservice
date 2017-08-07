52b18bdd0b3c65231e3b79c4ea9cce054d9b2b92
System <none>
2017-08-07 06:37:57 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -16,21 +16,26 @@ app.route('/')
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
 
-// app.route('/new:url')
-//     .get(function(req, res){
-//       res.send(req.params.url);
-// })
 
 app.get('/new/:url*', function(req, res){
   
   var wrongURL = ({error: 'Wrong url format, make sure you have a valid protocol and real site.'});
-  var validURL = req.params.url.length == 0 ? res.json(wrongURL) : req.params[0].length == 0 ? res.json(wrongURL): req.params.url + req.params[0];
+  var validURL = ''
+  
+  validateUrl(req.params.url + req.params[0]) ? 
+    validURL = req.params.url + req.params[0] :
+  res.json(wrongURL)
+  
+  
   
   
   
   res.send(validURL);
 })
 
+function validateUrl(value) {
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
 
 app.listen(process.env.PORT, function () {
   console.log('Node.js listening ...');
